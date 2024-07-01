1126a60709df75b38a6131af67a676ebb910c499
Alexis Guzmán <alexisuguzman11@gmail.com>
2024-07-01 23:07:07 +0000

    Save progress: 1470.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -80,6 +80,7 @@ AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available =
 RETURN_MENU () {
   #get customer info
   echo -e "\nWhat's your phone number?"
+  read PHONE_NUMBER
   #if not found
   #send to main menu
 }
